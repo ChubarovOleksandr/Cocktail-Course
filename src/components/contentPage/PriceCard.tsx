a6dcfecc6de76a8e655227ef9a27e1660be4463e
ChubarovOleksandr <o.chubarov@finik.pro>
2025-02-07 01:44:07 +0200

    fix button

--- src/components/contentPage/PriceCard.tsx
+++ src/components/contentPage/PriceCard.tsx
@@ -5,29 +5,29 @@ interface props {
 }
 
 const PriceCard = ({ data }: props) => {
-  return <div className={data.recommended ? "card light-bg" : "card"}>
-    <div className="card__header">
-      <span>{data.title}</span>
-    </div>
-    <div className="card__body">
-      {/* {data.title == 'Старт'
+  return (
+    <div className={data.recommended ? "card light-bg" : "card"}>
+      <div className="card__header">
+        <span>{data.title}</span>
+      </div>
+      <div className="card__body">
+        {/* {data.title == 'Старт'
       ? <span>$<strong>{data.price}</strong> / месяц</span>
       : <span>$<strong>{data.price}</strong></span>} */}
-      <span className="subtitle">
-        {data.subtitle}
-      </span>
-      <p className="subdescription">{data.description}</p>
-    <ul>
-      {data.advantages.map((advantageText, index) => (
-        <li key={index}>{advantageText}</li>
-      ))}
-    </ul>
+        <span className="subtitle">{data.subtitle}</span>
+        <p className="subdescription">{data.description}</p>
+        <ul>
+          {data.advantages.map((advantageText, index) => (
+            <li key={index}>{advantageText}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="card__footer">
+        <a href="https://t.me/roles_requests_bot">Получить</a>
+        {data.recommended && <span>( Рекомендуемый )</span>}
+      </div>
     </div>
-    <div className="card__footer">
-      <button>Получить</button>
-      {data.recommended && <span>( Рекомендуемый )</span>}
-    </div>
-  </div>;
+  );
 };
  
 export default PriceCard;
